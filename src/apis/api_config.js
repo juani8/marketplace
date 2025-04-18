f41f27c8943ab09fb96ef91da17cdf3bdab597b1
Juan Ignacio Sosa <juanisosa442@gmail.com>
2025-04-18 16:20:28 -0300

    Bug fix en conexión al backend

--- src/apis/api_config.js
+++ src/apis/api_config.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.BACKEND_URL || 'http://localhost:3000',
+  baseURL: process.env.REACT_APP_BACKEND_URL || 'http://localhost:3000',
   withCredentials: true
 });
 
