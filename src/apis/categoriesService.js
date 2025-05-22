c345e4aa31f800020677705c8d78db5360ccef70
ErikaYeon <erikayeon01@gmail.com>
2025-05-22 18:38:11 -0300

    categories select

--- src/apis/categoriesService.js
+++ src/apis/categoriesService.js
@@ -1,6 +1,6 @@
 import api from './api_config';
 
 export const getAllCategories = async () => {
-  const res = await api.get('/git');
-  return res.data; // El backend devuelve un array directo
+  const response = await api.get('/categories');
+  return response.data;
 };
