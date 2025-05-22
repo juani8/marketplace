b8c52b6be281e563e3616b810b2b09b547e6b408
ErikaYeon <erikayeon01@gmail.com>
2025-05-22 18:42:15 -0300

    updated for productTable

--- src/components/TableActions.jsx
+++ src/components/TableActions.jsx
@@ -3,11 +3,19 @@ import { FaEdit, FaTrash } from 'react-icons/fa';
 
 export default function TableActions({ onEdit, onDelete }) {
   return (
-    <div className="flex items-center gap-2">
-      <button onClick={onEdit} className="text-green-500 hover:text-green-700">
+    <div className="flex items-center gap-3">
+      <button
+        onClick={onEdit}
+        className="text-green-500 hover:text-green-700 text-l"
+        title="Editar"
+      >
         <FaEdit />
       </button>
-      <button onClick={onDelete} className="text-red-500 hover:text-red-700">
+      <button
+        onClick={onDelete}
+        className="text-red-500 hover:text-red-700 text-l"
+        title="Eliminar"
+      >
         <FaTrash />
       </button>
     </div>
