2acb3c629afd108b0567297a7dd75c7ee7b4c842
ErikaYeon <erikayeon01@gmail.com>
2025-05-22 18:47:41 -0300

    localhost:300 for testing

--- src/apis/api_config.js
+++ src/apis/api_config.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL, // 
+  baseURL: "http://localhost:3000/api", // 'https://marketplace-services-v8vn.onrender.com/api'
   withCredentials: true,
 });
 
