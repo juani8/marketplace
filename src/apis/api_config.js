51e80ad858a0852f0619cca1d340c91a18c49aa5
Juana <jotacaos2002@gmail.com>
2025-05-28 11:45:27 -0300

    added backend connection

--- src/apis/api_config.js
+++ src/apis/api_config.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/api", // 'https://marketplace-services-v8vn.onrender.com/api'
+  baseURL: "https://marketplace-services-v8vn.onrender.com/api",
   withCredentials: true,
 });
 
