c395a6b6015b252e8d9263eaad2378739cfd8bb0
juani8 <110401016+juani8@users.noreply.github.com>
2025-05-30 00:29:05 -0300

    Update api_config.js

--- src/apis/api_config.js
+++ src/apis/api_config.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: "https://marketplace-services-v8vn.onrender.com/api",
+  baseURL: import.meta.env.VITE_BACKEND_URL, // baseURL:'http://localhost:3000', Para pruebas locales
   withCredentials: true,
 });
 
