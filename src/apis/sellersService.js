85b3b46b7ceae558c911d43bd699967f3872f03a
ErikaYeon <erikayeon01@gmail.com>
2025-06-25 21:16:27 -0300

    temporal other sellerService

--- src/apis/sellersService.js
+++ src/apis/sellersService.js
@@ -1,7 +1,7 @@
 import api from './api_config';
 
 // Obtener los comercios visibles para el usuario logueado (admin u operador)
-export const getSellersByUsuario = async (usuarioId) => {
-  const response = await api.post('/sellers/list', { usuario_id: usuarioId });
+export const getSellersByUsuario = async (usuario_id) => {
+  const response = await api.post('/sellers/list', { usuario_id });
   return response.data.data;
 };
